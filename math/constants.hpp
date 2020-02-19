7a9c298fe916575925e60facf4651701f50cd10f
bermejus <davidbermejosanta@gmail.com>
2020-02-19 16:29:32 +0100

    Added pi and e constants

--- math/constants.hpp
+++ math/constants.hpp
@@ -5,5 +5,11 @@
 namespace math
 {
 	template <typename T>
+	constexpr T e = 2.7182818284590452353602875;
+
+	template <typename T>
+	constexpr T pi = 3.141592653589793238462643383279;
+
+	template <typename T>
 	constexpr T epsilon = std::numeric_limits<T>::epsilon();
 }
