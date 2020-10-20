b3045fc00599348603b134e01b9adfe8810f7f8d
David Bermejo <davidbermejosanta@gmail.com>
2020-10-20 16:23:56 +0200

    Cast constant before storing its value

--- math/constants.hpp
+++ math/constants.hpp
@@ -5,10 +5,10 @@
 namespace gopt
 {
 	template <typename T = double>
-	constexpr T e = 2.7182818284590452353602875;
+	constexpr T e = static_cast<T>(2.7182818284590452353602875);
 
 	template <typename T = double>
-	constexpr T pi = 3.141592653589793238462643383279;
+	constexpr T pi = static_cast<T>(3.141592653589793238462643383279);
 
 	template <typename T = double>
 	constexpr inline T epsilon(const T& x) { return std::nextafter(x, x + 0.1) - x; }
