f935cbc25619f86bb4ba6204b0ec356b6c61af55
David Bermejo <davidbermejosanta@gmail.com>
2020-12-06 12:45:47 +0100

    Removed old build script

--- build.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host "Building..."
-Set-Location build
-& "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\Llvm\bin\clang++.exe" --std=c++2a -O3 ../main.cpp
-Set-Location ..
-Write-Host "Done."
-Write-Host ""
-Write-Host "Executing program..."
-Write-Host ""
-./build/a.exe
